e967db2aa962875f3e5b49043bd62b8394005c0c
Batista Tech <rafildodev@gmail.com>
2024-06-24 10:02:32 -0300

    4

---
+++ lib/features/show_news/presentation/components/news_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/constants/palette.dart';
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 300,
+        width: MediaQuery.of(context).size.width - (2 * 16),
+        child: Stack(children: [
+          Container(
+            width: double.infinity,
+            height: 260,
+            margin: const EdgeInsets.only(
+              bottom: 16,
+            ),
+            color: Palette.lightGrey,
+            child: Image.network(
+              "https://sm.ign.com/t/ign_br/screenshot/default/imagem-2024-05-08-180800116_wtsk.1920.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 0,
+            child: Container(
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 2),
+                    blurRadius: 4,
+                    color: Colors.black12,
+                  ),
+                ],
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    'News Title',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: TextStyle(
+                      color: Palette.deepBlue,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/features/show_news/presentation/home_page.dart
+++ lib/features/show_news/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/constants/palette.dart';
+import 'package:news_app/features/show_news/presentation/components/news_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,17 +21,53 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: const [
-            TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(width: 1, color: Palette.lightGrey,),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TextField(
+              cursorColor: Palette.deepBlue,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Palette.lightGrey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Palette.deepBlue,
+                  ),
+                ),
               ),
-            ),            
-          ),
-        ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              "Top News",
+              style: TextStyle(
+                color: Palette.deepBlue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const NewsCard();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
